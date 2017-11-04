9a8b2bf2c66187280672dd96ec6112f9b07857db
Sanju3001 <codersaj@gmail.com>
2017-11-04 17:11:33 -0400

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import kids from './Kids_Transparent_Logo.png'
-import './App.css';
+
+import Choice from './Choice';
 
 class App extends Component {
+
+  state = {
+    questions: 'Someone is being mean to me at school - what should I do?'
+  };
+
+
+  pickOption = () => {
+    this.setState(() => ({ questions: 'Next question' }));
+  };
+
+
+
   render() {
     return (
       <div className="App">
@@ -14,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           Welcome!
         </p>
+        <Choice questions={this.state.questions} pickOption={this.pickOption} />
         <span className="skype-button bubble " data-contact-id="sajith.bandaranayake"></span>
       </div>
     );
